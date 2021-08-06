965d453ba3fe95a05b8ebe0a10a38a98781087e0
saiful islam <saiful01636758237@gmail.com>
2021-08-06 10:35:21 +0600

    composer install and setup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ce55180fe76bff7631187aec21f031e2862d8e2f',
+    'name' => 'saifulseip/edu-board',
+  ),
+  'versions' => 
+  array (
+    'saifulseip/edu-board' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ce55180fe76bff7631187aec21f031e2862d8e2f',
+    ),
+  ),
+);
