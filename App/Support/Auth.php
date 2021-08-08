6d3b82ade8f409b0a90cdb81bde2759de4942965
saiful islam <saiful01636758237@gmail.com>
2021-08-08 18:05:48 +0600

    dashboard tamplates slice done

--- App/Support/Auth.php
+++ App/Support/Auth.php
@@ -27,6 +27,15 @@
 			if ($num == 1 ){
 
 				if (password_verify($pass, $login_user_data['pass']) ) {
+
+					$_SESSION['id'] = $login_user_data['id'];
+					$_SESSION['role'] = $login_user_data['role'];
+					$_SESSION['name'] = $login_user_data['name'];
+					$_SESSION['uname'] = $login_user_data['uname'];
+					$_SESSION['email'] = $login_user_data['email'];
+					$_SESSION['cell'] = $login_user_data['cell'];
+					$_SESSION['photo'] = $login_user_data['photo'];
+
 					header('location:dashboard.php');
 				}else{
 					return "<p class=\"alert alert-warning\"> Wrong Password! <button class=\"close\" data-dismiss=\"alert\">&times;</button></p>";
